93b99495058bad0828f47b4b2fa11dae605395fb
Mommyprogrammer <lfrances@student.umgc.edu>
2024-05-14 07:23:06 -0700

    Add files via upload

---
+++ Japanese Haiku cs105 #1.py
@@ -0,0 +1,20 @@
+# Japanese Haiku algorithm
+
+# 5 syllabuls on line1, 8 syllabuls on line 2, and 5 syllabals on line3
+line1= 'The moon comes gently,'
+line2= 'on the empty side of the beach,'
+line3= 'another autumn.'
+
+# test output
+print(line1)
+print(line2)
+print(line3)
+
+# adding Title and authur's name
+haiku_title= 'haiku of the day'.title()
+author= 'steliana cristina voicu'.title()
+
+# test output
+print(haiku_title)
+print('by ' + author)
+
